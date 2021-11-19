579466d7e0d62085c93938e3244d43d621e5ded0
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-11-19 10:34:08 -0300

    date added to post card in post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,32 @@
 class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show new create]
+
+   def index
+    #if params[:query_ship]
+     # @posts = Occurrence.search_by_ship(params[:query_ship])
+    #else
+      @posts = Post.all
+    #end
+  end
+
+  def show
+    @post = List.find(params[:id])
+    #@bookmark = Bookmark.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    #redirect_to lists_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
