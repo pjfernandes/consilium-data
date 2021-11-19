4ec35f716521d0b4d2ada88efa98af97e7d31760
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-11-19 15:04:02 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Post.create(content: "asasadsddddddddddddddddddddddd
+    sasadsddddddddddddddddddddddd
+    sasadsddddddddddddddddddddddd
+    sasas",title:"asasasasasas", user_id:1)
+end
