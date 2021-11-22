f41b930b70719a9769a13df36047ae2b859ef661
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-11-21 23:41:10 -0300

    frontend adjusts

--- app/javascript/trix-editor-overrides.js
+++ app/javascript/trix-editor-overrides.js
@@ -1,6 +1,6 @@
 // app/javascript/trix-editor-overrides.js
 window.addEventListener("trix-file-accept", function (event) {
-  const acceptedTypes = ['image/jpeg', 'image/png']
+  const acceptedTypes = ['image/jpg','image/jpeg', 'image/png']
   if (!acceptedTypes.includes(event.file.type)) {
     event.preventDefault()
     alert("Only support attachment of jpeg or png files")
