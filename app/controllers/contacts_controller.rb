e556dc4019a3168e9b99225315707e3ddfec4572
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-12-01 08:42:33 -0300

    redirect after send email removed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
-      redirect_to root_path
+      #redirect_to root_path
     else
       flash.now[:error] = 'Cannot send message.'
       render :new
