a6858ed7e6a6c56f15142ff6f0cc7b8aa9ac3091
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-12-03 17:56:42 -0300

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,13 +110,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'https://consiliumdata.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'consiliumdata.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
 
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'consiliumdata.herokuapp.com',
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
