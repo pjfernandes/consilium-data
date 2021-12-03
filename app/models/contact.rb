35d4f826a405c7ff07834079dbb0e46a5e40858d
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-12-03 18:04:21 -0300

    done

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -10,7 +10,7 @@ class Contact < MailForm::Base
   def headers
     {
       :subject => "Consilium Data Contact Form",
-      :to => "pj_fernandes@id.uff.br",
+      :to => "pjf_fernandes@yahoo.com.br",
       :from => %("#{name}" <#{email}>)
     }
   end
