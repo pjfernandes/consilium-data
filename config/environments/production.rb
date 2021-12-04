86d233d8b37ba60b4fdbde2abf650ac81cb1b217
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-12-03 22:48:31 -0300

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,7 +113,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'consiliumdata.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
