d92f34cd1199009eac42030cb3d8a7dc691b7594
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-12-08 12:43:09 -0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  get 'contact/new'
+  #get 'contact/new'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available withinthis file, see https://guides.rubyonrails.org/routing.html
 
-  resources :messages
+  resources :messages, only: %i[index show new create]
 
   resources :posts do
     resources :comments
   end
 
-  match '/contacts', to: 'contacts#new', via: 'get'
-  resources :messages, only: [:new, :create, :index, :show]
+  #match '/contacts', to: 'contacts#new', via: 'get'
+  #resources :contacts, only: [:new, :create]
 
 end
