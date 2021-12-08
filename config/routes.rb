10fe80fc302e0936a89e8e59f0eeaf2b0c06293c
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2021-12-08 11:20:13 -0300

    messages added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available withinthis file, see https://guides.rubyonrails.org/routing.html
 
   resources :posts do
     resources :comments
